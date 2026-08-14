b2de9768985d6e21db90de578e5c7b0ae48019c2
Sahil Khuswaha <sahilk09422@gmail.com>
2026-08-14 23:41:37 +0530

    Add health check API

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -7,3 +7,9 @@ def home():
     return {
         "message": "Welcome to LeetCode Company Tracker API 🚀"
     }
+
+@app.get("/health")
+def health():
+    return {
+        "status": "Backend is running"
+    }
