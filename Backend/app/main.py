2c7ebaf7b8a6132e19300402d7e9c1674c9be485
Sahil Khuswaha <sahilk09422@gmail.com>
2026-08-30 22:11:20 +0530

    Add company detail API

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def home():
     return {
         "message": "Welcome to LeetCode Company Tracker API 🚀"
     }
+
 
 @app.get("/health")
 def health():
@@ -14,8 +16,17 @@ def health():
         "status": "Backend is running"
     }
 
+
 @app.get("/companies")
 def get_companies():
     return {
         "companies": ["Google", "Microsoft", "Amazon"]
+    }
+
+
+@app.get("/companies/{company_name}")
+def get_company(company_name: str):
+    return {
+        "company": company_name,
+        "message": "Company found"
     }
